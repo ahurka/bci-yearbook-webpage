a83587accabee6afb2cb93f29332d2c74614d781
ahurka <hurka.alex@gmail.com>
2018-05-08 22:44:31 -0400

    Initialize React project

---
+++ project-setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+npm add react react-dom
+npm add react-scripts
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+console.log("Hello?");
+ReactDOM.render(
+  React.createElement("p", null, "Default Text"),
+  document.getElementById("root")
+);
